982aa207c7db22b99ccfb3fd5cce9fe7a0f274f4
sraymond2020 <raymond.shaun@gmail.com>
2022-05-24 00:31:49 -0400

    update

---
+++ chromecast.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+#  Copyright (C) 2015-2016 Ben Klein. All rights reserved.
+#
+#  This application is licensed under the GNU GPLv3 License, included with
+#  this application source.
+
+import sys
+
+from PyQt6 import QtWidgets
+
+global DEBUG
+DEBUG = True
+
+if __name__ == '__main__':
+    print("Use main.py instead.")
+    sys.exit();
+
+# Qt GUI stuff
+try:
+    from PyQt6 import QtCore, QtGui
+    from PyQt6.QtCore import QSettings
+except ImportError:
+    print("There was an error importing the Qt python3 libraries,")
+    print("These are required by to operate this program.")
+    print("If you are on Ubuntu/Debian, they should be available via APT.")
+    sys.exit("Could not import Python3 Qt Libraries.")
+
+
+class AirplayDevice():
+    def __init__():
+        print("")
